7878ea8d3faa9884678ee05b22726213396059aa
Daniel <vychydan@fit.cvut.cz>
2023-11-11 20:16:07 +0100

    Fix dir recursion comp_type problem

--- src/directoryContentsComparator.cpp
+++ src/directoryContentsComparator.cpp
@@ -15,23 +15,25 @@
 namespace fs = std::filesystem;
 
 bool isFileAndDirectory( const std::string &path1, const std::string &path2 ) {
-    bool isPath1File = fs::is_regular_file( path1 );
+    bool isPath1File      = fs::is_regular_file( path1 );
     bool isPath1Directory = fs::is_directory( path1 );
-    bool isPath2File = fs::is_regular_file( path2 );
+    bool isPath2File      = fs::is_regular_file( path2 );
     bool isPath2Directory = fs::is_directory( path2 );
 
-    return ( isPath1File && isPath2Directory ) || ( isPath1Directory && isPath2File );
+    return ( isPath1File && isPath2Directory ) ||
+           ( isPath1Directory && isPath2File );
 }
 
-directoryContentsComparator::directoryContentsComparator( const std::string &directory_1,
-                                                          const std::string &directory_2 )
+directoryContentsComparator::directoryContentsComparator(
+    const std::string &directory_1, const std::string &directory_2 )
     : Comparator( directory_1, directory_2 ) {
 
     std::vector<std::string> contents_1;
     std::vector<std::string> contents_2;
 
     for ( const auto &file : fs::directory_iterator( _name_1 ) )
-        contents_1.push_back( file.path().string() ); // Can be file or directory
+        contents_1.push_back(
+            file.path().string() ); // Can be file or directory
     for ( const auto &file : fs::directory_iterator( _name_2 ) )
         contents_2.push_back( file.path().string() );
 
@@ -43,17 +45,22 @@ void directoryContentsComparator::findDifferences() {
     if ( _directory_1.second.size() != _directory_2.second.size() )
         _diff_count++;
 
-    for ( auto item = _directory_1.second.begin(); item != _directory_1.second.end(); item++ ) {
+    for ( auto item = _directory_1.second.begin();
+          item != _directory_1.second.end(); item++ ) {
         bool found_counterpart = false;
         for ( auto counterpart = _directory_2.second.begin();
               counterpart != _directory_2.second.end(); counterpart++ ) {
             if ( isFileAndDirectory( *item, *counterpart ) )
                 continue;
-            const char *items[] = { "", ( ( *item ).c_str() ), ( ( *counterpart ).c_str() ) };
+            // Manually 're-enter' console arguments to recursively start
+            // another comparison process for the directory
+            const char *items[] = { "ediff", "-contents", ( ( *item ).c_str() ),
+                                    ( ( *counterpart ).c_str() ) };
 
-            App app( 3, items );
+            App app( 4, items );
             size_t comp_type;
-            // By runnign getCompType() a directory comparator can be called again
+            // By runnign getCompType() a directory comparator can be called
+            // again
 
             try {
                 comp_type = app.getCompType();
@@ -63,7 +70,7 @@ void directoryContentsComparator::findDifferences() {
             std::cout << comp_type << " ";
 
             Comparator *comparator = nullptr;
-            comparator = app.startComparison( comp_type );
+            comparator             = app.startComparison( comp_type );
 
             if ( comparator != nullptr ) {
                 comparator->findDifferences();
@@ -81,7 +88,8 @@ void directoryContentsComparator::findDifferences() {
         }
 
         if ( !found_counterpart )
-            // This means that one or more files have not found any matches -> directories are not equal
+            // This means that one or more files have not found any matches ->
+            // directories are not equal
             _diff_count++;
     }
 }
